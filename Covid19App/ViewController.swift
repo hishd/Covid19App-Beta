b09af659746219479c8bfd55b1f9434566cc8dcf
hishd <hish2k15@gmail.com>
2020-09-14 11:50:49 +0530

    Initial Commit

---
+++ Covid19App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19App
+//
+//  Created by Hishara on 9/14/20.
+//  Copyright © 2020 Hishara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
